5be99b2044bd5f96acbf0771c198fd4a2ec8c8f7
Roman Kurochkin <romankurockin473@gmail.com>
2023-02-03 16:51:20 +0200

    was added some functional

--- scripts/toDoList.js
+++ scripts/toDoList.js
@@ -1,53 +1,44 @@
-window.addEventListener('load',() =>{
-    const form = document.querySelector("#new-task-form");
-    const input = document.querySelector("#new-task-input");
-    const list_el = document.querySelector("#tasks");
 
-    form.addEventListener('submit',(e) => {
-        e.preventDefault();
+let addMessage = document.querySelector('.message'),
+    addButton = document.querySelector('.add');
+    todo = document.querySelector('.todo');
 
-        const task = input.value;
-        if(!task){
-            alert("Fill out the task");
-            return;
-        }
+let todoList = [];
 
-        const task_el = document.createElement("div");
-        task_el.classList.add("task");
+if(localStorage.getItem('todo')){
+    todoList = JSON.parse(localStorage.getItem('todo'));
+    displayMessages();
+}
 
-        const task_content_el = document.createElement("div");
-        task_content_el.classList.add("content")
-        task_content_el.innerHTML = task;
-       
-        task_el.appendChild(task_content_el);
+addButton.addEventListener('click',function(){
 
-        const task_input_el = document.createElement("input");
-        task_content_el.classList.add("text");
-        task_input_el.type = "text";
-        // task_input_el.setAttribute("readonly","readonly");
+    let newTodo = {
+        todo: addMessage.value,
+        checked: false,
+        important: false
+    };
 
-        task_content_el.appendChild(task_input_el);
+    todoList.push(newTodo);
+    displayMessages();
+    localStorage.setItem('todo',JSON.stringify(todoList));
+    });
 
-        const task_action_el = document.createElement("div");
-        task_action_el.classList.add("actions");
-
-        const task_delete_el = document.createElement("button");
-        task_delete_el.classList.add("delete");
-        task_delete_el.innerHTML = "delete";
-
-        task_action_el.appendChild(task_delete_el);
-
-        task_el.appendChild(task_action_el);
-
-        list_el.appendChild(task_el);
-        
-        input.value = "";
-
-        task_delete_el.addEventListener('click',() => {
-            list_el.removeChild(task_el);
+    function displayMessages(){
+        let displayMessage =""
+        todoList.forEach(function(item,i){
+            displayMessage += `
+            <li>
+                <input type="checkbox" id="item_${i}" ${item.checked ? 'checked' : ''}>
+                <label for="item_${i}">${item.todo}</label>       
+            </li>
+            `;
+            todo.innerHTML = displayMessage;
         })
-
-    })
-
-
-} )
+    }
+todo.addEventListener('change',function(event){
+    let idInput = event.target.getAttribute('id');
+    let forLabel = todo.querySelector('[for='+ idInput +']');
+    let valueLabel = forLabel.innerHTML;
+    console.log('valuelabel 22:19:',valueLabel);
+    
+})
